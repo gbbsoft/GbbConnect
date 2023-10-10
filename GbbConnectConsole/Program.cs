aa7cff5c0eeee8a83ccc84955e595a894c78323c
Piotr Maciejewski <{ID}+{username}@users.noreply.github.com>
2023-10-10 17:13:01 +0200

    1. Changes in UI

--- GbbConnectConsole/Program.cs
+++ GbbConnectConsole/Program.cs
@@ -13,19 +13,30 @@ namespace GbbConnectConsole
         /// <param name="DontWaitForKey">don't wait for key, but just wait forever</param>
         static void Main(bool DontWaitForKey)
         {
+            Console.WriteLine();
+            Console.WriteLine("GbbConnectConsole by gbbsoft");
+            Console.WriteLine();
 
             Console.WriteLine($"Parameters file            : {GbbEngine.Configuration.Parameters.Parameters_GetFileName()}");
             Console.WriteLine($"Log and statistic directory: {GbbEngine.Configuration.Parameters.OurGetUserBaseDirectory()}");
+            Console.WriteLine();
 
             // load parameters
             var FileName = GbbEngine.Configuration.Parameters.Parameters_GetFileName();
-            var Parameters = GbbEngine.Configuration.Parameters.Load(FileName);
+            if (!File.Exists(FileName))
+            {
+                Console.WriteLine("ERROR: No parameters.xml file!");
+                DontWaitForKey = false;
+            }
+            else
+            {
+                var Parameters = GbbEngine.Configuration.Parameters.Load(FileName);
 
-            // start server
-            var JobManeger = new GbbEngine.Server.JobManager();
-            var Log = new Log();
-            JobManeger.OurStartJobs(Parameters, Log);
-
+                // start server
+                var JobManeger = new GbbEngine.Server.JobManager();
+                var Log = new Log();
+                JobManeger.OurStartJobs(Parameters, Log);
+            }
 
             if (DontWaitForKey)
             {
@@ -34,6 +45,7 @@ namespace GbbConnectConsole
             else
             {
                 // Finish
+                Console.WriteLine();
                 Console.WriteLine("Press Enter to finish application...");
                 Console.ReadLine();
                 Console.WriteLine("Goodbye!");
