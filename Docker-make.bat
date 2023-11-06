e2a5b5f8030c93ea35d56bcc7244224b37e536ff
Piotr Maciejewski <{ID}+{username}@users.noreply.github.com>
2023-11-06 09:39:43 +0100

    1. Docker-make: change name of image

--- Docker-make.bat
+++ Docker-make.bat
@@ -1,4 +1,4 @@
-docker build . -t gbbconnectconsole
+docker build . -t gbbconnectimage
 pause Press any key to run 
-docker run  -i --name gbbconnectconsole gbbconnectconsole
+docker run  -i --name gbbconnectconsole gbbconnectimage
 pause
